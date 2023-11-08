c13eaaa0dc62bc9bec9946a7be56ebd166327d46
viktoria wallström <viktoria-wallstrom@hotmail.com>
2023-11-08 15:45:57 +0100

    Lagt till egenskaper och en metod

--- BookClass.cs
+++ BookClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,20 @@ namespace BokSystem
 {
     public class BookClass
     {
-        public int Name { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Isbn { get; set; }
+
+        public BookClass(string title, string author, int isbn)
+        {
+            Title = title;
+            Author = author;
+            Isbn = isbn;
+        }
+
+        public void WriteBookInfo()
+        {
+            Console.WriteLine($"Titel: {Title}, Författare: {Author}, ISBN-nummber: {Isbn}");
+        }
     }
 }
